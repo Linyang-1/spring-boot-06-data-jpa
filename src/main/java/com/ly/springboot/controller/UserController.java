a9cc1053892fc4d7bac9ae115ec8fbe7f9a79cab
LY <13700987352@163.com>
2020-02-23 14:27:52 +0800

    设置id为2

--- src/main/java/com/ly/springboot/controller/UserController.java
+++ src/main/java/com/ly/springboot/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    int id = 1;
+    int id = 2;
 
     @Autowired
     UserRepository userRepository;
